ba16a6538bd277237cff44e9ec2722b80b1de740
Chris <cbiegay@gmail.com>
2021-10-15 09:38:28 -0500

    Load employees from a file

--- pages/employees/list.js
+++ pages/employees/list.js
@@ -1,6 +1,21 @@
 import EmployeeLayout from "../../components/employeeLayout"
+import fs from 'fs'
+import path from 'path'
 
-export default function ListEmployees() {
+const employeesFile = path.join(process.cwd(), "resources/employee-data.json")
+
+export async function getServerSideProps(/* context */) {
+  const fileContents = fs.readFileSync(employeesFile, 'utf8')
+  const employees = JSON.parse(fileContents)
+
+  return {
+    props: {
+      employees
+    }
+  }
+}
+
+export default function ListEmployees({ employees }) {
   return (
     <EmployeeLayout pageTitle="List Employees">
       <h1>Employees</h1>
@@ -12,14 +27,18 @@ export default function ListEmployees() {
               <th>Department</th>
               <th>Actions</th>
           </tr>
-          <tr>
-              <td>0</td>
-              <td>Static Test</td>
-              <td>Engineering</td>
-              <td>
-                  <a href="">Delete</a>
-              </td>
-          </tr>
+          {
+            employees.map(({id, name, department}) => (
+              <tr>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{department}</td>
+                  <td>
+                      <a href="">Delete</a>
+                  </td>
+              </tr>
+            ))
+          }
       </table>
     </EmployeeLayout>
   )
